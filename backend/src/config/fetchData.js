89d90f6df70a1a670916bd35b64b21c9a9412ec2
lopesboa <lopesboa1@gmail.com>
2020-01-16 23:46:39 -0300

    fix: solving problem with data fetch class

--- backend/src/config/fetchData.js
+++ backend/src/config/fetchData.js
@@ -3,21 +3,26 @@ const api = require('./api')
 
 class FetchData {
   constructor () {
-    this.resultCount = 1
-    this.onPage = 1
     this.allJobs = []
-    this.data()
+    this.resultCount = 1
+    this.onPage = 0
+
+    this.getData()
   }
 
-  async data () {
+  async getData () {
     try {
       while (this.resultCount > 0) {
         const response = await api.get(`?page=${this.onPage}`)
         const jobsResults = response.data
+
         this.allJobs.push(...jobsResults)
         this.resultCount = jobsResults.length
+        // console.log(`Got, ${this.resultCount}, jobs`)
+
         this.onPage++
       }
+      // console.log(`Got, ${this.allJobs.length}, jobs total`)
     } catch (error) {
       console.log(`error, data not fetched ${error}`)
     }
